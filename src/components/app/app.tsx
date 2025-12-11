16e6791948741bd7bbaa4a5ee0a37f7d58f16502
Николай Бойков <nickboickov@yandex.ru>
2025-12-11 21:30:15 +0300

    PR11-appRoute-appHeaderLink

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,36 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-
-import { AppHeader } from '@components';
+import { Route, Routes } from 'react-router-dom';
+import { OrderInfo } from '../order-info';
+import { IngredientDetails } from '../ingredient-details';
 
 const App = () => (
   <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/feed/:numder' element={<OrderInfo />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-profile' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='/ingredients/:id' element={<IngredientDetails />} />
+      <Route path='/profile/orders/:number' element={<OrderInfo />} />
+      <Route path='*' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
-
 export default App;
