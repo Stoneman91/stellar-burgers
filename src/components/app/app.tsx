852cbe5706a116dcb7167520339e49a277dc2a46
Николай Бойков <nickboickov@yandex.ru>
2025-12-12 11:26:04 +0300

    PR11-ordersSlice-feedSlice

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -11,24 +11,60 @@ import {
 } from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation, useNavigate } from 'react-router-dom';
 import { OrderInfo } from '../order-info';
 import { IngredientDetails } from '../ingredient-details';
+import { Modal } from '../modal';
+import { useDispatch } from '../../services/store';
+import { useEffect } from 'react';
+import { getIngredients } from 'src/slices/ingredientsSlice';
+const location = useLocation();
+const background = location.state?.background;
+const navigate = useNavigate();
+
+useEffect(() => {
+  useDispatch(getIngredients());
+}, []);
+
+const handleCloseModal = () => {
+  navigate(-1);
+};
 
 const App = () => (
   <div className={styles.app}>
     <Routes>
       <Route path='/' element={<ConstructorPage />} />
       <Route path='/feed' element={<Feed />} />
-      <Route path='/feed/:numder' element={<OrderInfo />} />
+      <Route
+        path='/feed/:numder'
+        element={
+          <Modal title='' onClose={handleCloseModal}>
+            <OrderInfo />{' '}
+          </Modal>
+        }
+      />
       <Route path='/login' element={<Login />} />
       <Route path='/register' element={<Register />} />
       <Route path='/forgot-profile' element={<ForgotPassword />} />
       <Route path='/reset-password' element={<ResetPassword />} />
       <Route path='/profile' element={<Profile />} />
       <Route path='/profile/orders' element={<ProfileOrders />} />
-      <Route path='/ingredients/:id' element={<IngredientDetails />} />
-      <Route path='/profile/orders/:number' element={<OrderInfo />} />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal title='' onClose={handleCloseModal}>
+            <IngredientDetails />{' '}
+          </Modal>
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <Modal title='' onClose={handleCloseModal}>
+            <OrderInfo />{' '}
+          </Modal>
+        }
+      />
       <Route path='*' element={<NotFound404 />} />
     </Routes>
   </div>
